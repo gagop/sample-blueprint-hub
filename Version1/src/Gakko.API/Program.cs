35b78fbe85d264a55e68ecda8e78c425a9b6b43d
Piotr Gago <gagopiotr@gmail.com>
2024-03-22 16:15:19 +0100

    Removed comments from Program.cs

--- Version1/src/Gakko.API/Program.cs
+++ Version1/src/Gakko.API/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -14,7 +12,6 @@ builder.Services.AddScoped<IRecruitmentsService, RecruitmentsService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
